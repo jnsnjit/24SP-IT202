45f50420c84ea4890ebb6278a5885a20d2e496cd
jnsnjit <156932213+jnsnjit@users.noreply.github.com>
2024-01-17 10:53:16 -0500

    update my_goal.php to actually have a goal yay

--- my_goal.php
+++ my_goal.php
@@ -1,3 +1,5 @@
 <?php
- echo "My Goals for IT-202 is...";
+ echo "My Goals for IT-202 is... I am taking this course because I like IT
+      :D. I would like to refine my skills on javascript, as well as learn
+      how to use PHP and SQL.";
 ?>
