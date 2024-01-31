4a9d71ef00f092f14a8d2fa6c33309b9287a1d4f
jnsnjit <156932213+jnsnjit@users.noreply.github.com>
2024-01-31 10:31:49 -0500

    Future Value Calculator
    form and results calculator is done
    taken from class notes

--- phpChapter2/future_value_form.php
+++ phpChapter2/future_value_form.php
@@ -1,28 +1,38 @@
 <?php
-// slide 62
-//set default variables for initial page load
-if (!isset($investment)) { $investment = "";}
-if (!isset($interest_rate)) { $interest_rate = "";}
-if (!isset($years)) { $years = "";}
-
+  // Slide 62
+  if( !isset($investment)) { $investment = ''; }
+  if( !isset($interest_rate)) { $interest_rate = ''; }
+  if( !isset($years)) { $years = ''; }
 ?>
 <html>
-    <head>
-        <title>Future Value Calculator</title>
-    </head>
-    <body>
-        <h1>Future Value Calculator</h1>
-        <form action = "future_value_results.php" method = "post">
-            <label>Investment Amount: </label>
-            <input type="text" name="investment" value="<?php echo htmlspecialchars($investment)?>" />
-            <br>
-            <label>Interest Rate: </label>
-            <input type="text" name="interest_rate" value="<?php echo htmlspecialchars($interest_rate)?>" />
-            <br>
-            <label>Amount of Years: </label>
-            <input type="text" name="years" value="<?php echo htmlspecialchars($years)?>" />
-            <br>
-            <input type="submit" value="Calculate"/>
-        </form>
-    </body>
+  <head>
+    <title>Future Value Calculator</title>
+  </head>
+  <body>
+    <h1>Future Value Calculator</h1>
+    <?php
+      if( !empty($error_message) ) {
+        echo "<p>";
+        echo $error_message;
+        echo "</p>";
+      }
+    ?>
+    <!-- Slide 63 -->
+    <form action="future_value_results.php" method="post">
+      <label>Investment Amount:</label>
+      <input type="text" name="investment"
+        value="<?php echo htmlspecialchars($investment); ?>" />
+      <br>
+      <label>Yearly Interest Rate:</label>
+      <input type="text" name="interest_rate"
+        value="<?php echo htmlspecialchars($interest_rate); ?>" />
+      <br>
+      <label>Number of Years:</label>
+      <input type="text" name="years"
+        value="<?php echo htmlspecialchars($years); ?>" />
+      <br>
+      <!-- Slide 64 -->
+      <input type="submit" value="Calculate" />
+    </form>
+  </body>
 </html>
