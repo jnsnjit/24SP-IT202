7efbc601d5cc554db7abdd8510e0c98f9e0d3efa
jnsnjit <156932213+jnsnjit@users.noreply.github.com>
2024-02-02 11:15:32 -0500

    chapters 3,4,5
    html with css styling in new chapters3,4,5  folder
    can now start on semester project

---
+++ phpChapter3,4,5/html_and_css_example/footer.php
@@ -0,0 +1,12 @@
+<!-- embedded/internal style -->
+<style>
+    h5{
+        color:blanchedalmond;
+        background-color: #828489;
+    }
+</style>
+<footer color>
+    <h5>
+        <p>Name, Feb 2, 2024, IT-202-006, notmyrealemail@gmail.com</p>
+    </h5>
+</footer>
---
+++ phpChapter3,4,5/html_and_css_example/header.php
@@ -0,0 +1,6 @@
+<header>
+    <img src="html_image.png" alt= "bad image" width="50"/>
+    <!-- inline style -->
+    <h1 style="color:blanchedalmond; background-color: #828489">Course Name: Advanced Internet Application</h1>
+    <h2>Course Subject: IT-302 </h2>
+</header>
