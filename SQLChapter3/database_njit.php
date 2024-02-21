492434bfd3ed70c3a09c17a3f69bc94d19f5c841
jnsnjit <156932213+jnsnjit@users.noreply.github.com>
2024-02-21 11:27:53 -0500

    class notes from the last week
    uh yea

---
+++ SQLChapter3/database_njit.php
@@ -0,0 +1,13 @@
+<?php
+//slide 24
+$dsn = 'mysql:host=sql1.njit.edu;port=3306;dbname=jns';
+$username = 'jns';
+$password = 'IT202@jimsil';
+try{
+    $db = new PDO($dsn,$username,$password);
+    echo '<p>you are connected to the njit database!</p>';
+} catch(PDOException $ex) {
+    $error_message = $ex->getMessage();
+    include('database_error.php');
+    exit();
+}
